e0c46701b3b9faf9cfa3c01b6b725d4cb27c7d27
smilejakdu <ash982416@gmail.com>
2022-03-07 13:57:48 +0900

    PurchaseBrokeragePolicy refac

--- src/policy/BrokerageRule.ts
+++ src/policy/BrokerageRule.ts
@@ -1,8 +1,10 @@
 export class BrokerageRule {
+	private lessThan: number;
 	private percentBrokerage: number;
 	private limitAmount: number;
 
-	constructor(percentBrokerage: number, limitAmount: number) {
+	constructor(percentBrokerage: number, limitAmount: number, lessThan: number) {
+		this.lessThan = lessThan;
 		this.percentBrokerage = percentBrokerage;
 		this.limitAmount = limitAmount;
 	}
--- src/policy/PurchaseBrokeragePolicy.ts
+++ src/policy/PurchaseBrokeragePolicy.ts
@@ -2,20 +2,31 @@ import { BrokeragePolicy } from './BrokeragePolicy';
 import { BrokerageRule } from './BrokerageRule';
 
 export class PurchaseBrokeragePolicy implements BrokeragePolicy {
+	private rule: BrokerageRule[] = [];
+	public PurchaseBrokeragePolicy() {
+		this.rule.push(
+			new BrokerageRule(50000000, 0.6, 250000),
+			new BrokerageRule(200000000, 0.5, 800000),
+			new BrokerageRule(600000000, 0.4, null),
+			new BrokerageRule(900000000, 0.5, null),
+			new BrokerageRule(Math.max(), 0.9, null),
+		);
+	}
+
 	async createRule(price: number) {
-		let rule: BrokerageRule;
-		if (price < 50000000) {
-			rule = new BrokerageRule(0.6, 250000);
-		} else if (price < 200000000) {
-			rule = new BrokerageRule(0.5, 800000);
-		} else if (price < 600000000) {
-			rule = new BrokerageRule(0.4, null);
-		} else if (price < 900000000) {
-			rule = new BrokerageRule(0.5, null);
-		} else {
-			rule = new BrokerageRule(0.9, null);
-		}
-		return rule;
+		// let rule: BrokerageRule;
+		// if (price < 50000000) {
+		// 	rule = new BrokerageRule(50000000, 0.6, 250000);
+		// } else if (price < 200000000) {
+		// 	rule = new BrokerageRule(200000000, 0.5, 800000);
+		// } else if (price < 600000000) {
+		// 	rule = new BrokerageRule(600000000, 0.4, null);
+		// } else if (price < 900000000) {
+		// 	rule = new BrokerageRule(900000000, 0.5, null);
+		// } else {
+		// 	rule = new BrokerageRule(Math.max(price), 0.9, null);
+		// }
+		// return rule;
 	}
 
 	async calculate(price: number): Promise<number> {
